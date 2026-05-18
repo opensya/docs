3ed6651c2e0db007db53a94b1e609f9ee7eb7d96
Mamadou Talla DIA <ibntalla@gmail.com>
2026-05-18 17:15:09 +0000

    feat: Améliorer le composant AppLogo et mettre à jour le lien GitHub dans la documentation

--- app/components/AppLogo.vue
+++ app/components/AppLogo.vue
@@ -2,6 +2,12 @@
   <div class="flex items-center gap-2 group/logo select-none">
     <AppFavicon class="text-highlighted size-6" :arrow-stroke-width="1.8" />
 
-    <div class="font-light text-[22px] text-highlighted">OpenSya</div>
+    <div class="font-extralight text-[22px] text-highlighted">OpenSya</div>
+
+    <div
+      class="bg-inverted text-inverted font-extralight text-xs rounded-sm px-1"
+    >
+      Alpha
+    </div>
   </div>
 </template>
